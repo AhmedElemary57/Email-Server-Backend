008c278252198811e26198c8810e5e120907270d
Ahmed elemary <60315006+AhmedElemary57@users.noreply.github.com>
2022-10-06 17:35:23 +0200

    Update ServerController.java

--- src/main/java/controller/ServerController.java
+++ src/main/java/controller/ServerController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin
 @Controller
 public class ServerController {
 
